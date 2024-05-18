ea030933b1ef9a6b3aab24a415cf8cc074ef50ba
Pains1 <arosman002@st.ug.edu.gh>
2024-05-18 09:42:08 +0000

    next one

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,4 +1,14 @@
 function processArray(numbers) {
     return numbers.map((num) => (num % 2 === 0 ? num ** 2 : num * 3));
   }
+
+
+  function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((string, index) =>
+  processedNumbers[index] % 2 === 0
+  ? string.toUpperCase()
+        : string.toLowerCase()
+    );
+  }
+  
   
